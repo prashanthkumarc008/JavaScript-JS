50db043af50247524760eaf559c52f4e491a5ec9
Prashanth Kumar C <prashanthkumarc008@gmail.com>
2025-04-16 14:57:13 +0530

    Update data.js

--- Asynch/data.js
+++ Asynch/data.js
@@ -1,8 +1,6 @@
 let employees=[{eid:101,ename:'rahul',esal:45500},
                 {eid:102,ename:'modi',esal:25000},
-                {eid:103,ename:'ananda',esal:30000},
-
-                ]
+                {eid:103,ename:'ananda',esal:30000} ]
 let createEmployee=(emp)=>{
     setTimeout(() => {
         employees.push(emp)
